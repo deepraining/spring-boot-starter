ce04cdeb22ae84f72abf007412b62f2d05d7ad1d
senntyou <jiangjinbelief@163.com>
2020-04-23 09:48:08 +0800

    add sql/0.0.3

---
+++ sql/V0.0.2__Admin.sql
@@ -0,0 +1,162 @@
+-- ----------------------------
+-- Table structure for admin_user
+-- ----------------------------
+CREATE TABLE `admin_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `username` varchar(64) NOT NULL COMMENT '用户名',
+  `password` varchar(64) NOT NULL COMMENT '加密密码',
+  `avatar` varchar(500) DEFAULT NULL COMMENT '头像',
+  `email` varchar(100) DEFAULT NULL COMMENT '邮箱',
+  `nickname` varchar(200) DEFAULT NULL COMMENT '昵称',
+  `note` varchar(500) DEFAULT NULL COMMENT '备注信息',
+  `last_login_time` datetime DEFAULT NULL COMMENT '最后登录时间',
+  `status` int(1) DEFAULT 1 COMMENT '帐号启用状态：0->禁用；1->启用',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10000 DEFAULT CHARSET=utf8mb4 COMMENT='后台用户表';
+
+-- ----------------------------
+-- Table structure for admin_login_log
+-- ----------------------------
+CREATE TABLE `admin_login_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `admin_user_id` bigint(20) DEFAULT NULL,
+  `ip` varchar(64) DEFAULT NULL COMMENT 'ip地址',
+  `address` varchar(100) DEFAULT NULL COMMENT '地址',
+  `user_agent` varchar(100) DEFAULT NULL COMMENT '浏览器登录类型',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台用户登录日志表';
+
+-- ----------------------------
+-- Table structure for admin_role
+-- ----------------------------
+CREATE TABLE `admin_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL COMMENT '名称',
+  `description` varchar(500) DEFAULT NULL COMMENT '描述',
+  `admin_user_count` int(11) DEFAULT NULL COMMENT '后台用户数量',
+  `status` int(1) DEFAULT 1 COMMENT '启用状态：0->禁用；1->启用',
+  `sort` int(11) DEFAULT 0 COMMENT '排序值',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台用户角色表';
+
+-- ----------------------------
+-- Table structure for admin_permission
+-- ----------------------------
+CREATE TABLE `admin_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父级权限id',
+  `name` varchar(100) NOT NULL COMMENT '名称',
+  `value` varchar(200) NOT NULL COMMENT '权限值',
+  `icon` varchar(500) DEFAULT NULL COMMENT '图标',
+  `type` int(1) DEFAULT NULL COMMENT '权限类型：0->目录；1->菜单；2->按钮（接口绑定权限）',
+  `uri` varchar(200) DEFAULT NULL COMMENT '前端资源路径',
+  `status` int(1) DEFAULT 1 COMMENT '启用状态；0->禁用；1->启用',
+  `sort` int(11) DEFAULT 0 COMMENT '排序',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台用户权限表';
+
+-- ----------------------------
+-- Table structure for admin_user_role_relation
+-- ----------------------------
+CREATE TABLE `admin_user_role_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `admin_user_id` bigint(20) DEFAULT NULL,
+  `admin_role_id` bigint(20) DEFAULT NULL,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台用户和角色关系表';
+
+-- ----------------------------
+-- Table structure for admin_user_permission_relation
+-- ----------------------------
+CREATE TABLE `admin_user_permission_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `admin_user_id` bigint(20) NOT NULL,
+  `admin_permission_id` bigint(20) NOT NULL,
+  `type` int(1) DEFAULT NULL COMMENT '类型：1->增加权限，-1->减少权限',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台用户和权限关系表(除角色中定义的权限以外的加减权限)';
+
+-- ----------------------------
+-- Table structure for admin_role_permission_relation
+-- ----------------------------
+CREATE TABLE `admin_role_permission_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `admin_role_id` bigint(20) DEFAULT NULL,
+  `admin_permission_id` bigint(20) DEFAULT NULL,
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台用户角色和权限关系表';
+
+-- ----------------------------
+-- Table structure for admin_menu
+-- ----------------------------
+CREATE TABLE `admin_menu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父级ID',
+  `title` varchar(100) NOT NULL COMMENT '菜单名称',
+  `level` int(4) DEFAULT NULL COMMENT '菜单级数',
+  `sort` int(4) DEFAULT 0 COMMENT '菜单排序',
+  `name` varchar(100) DEFAULT NULL COMMENT '前端名称',
+  `icon` varchar(200) DEFAULT NULL COMMENT '前端图标',
+  `hidden` int(1) DEFAULT NULL COMMENT '前端隐藏',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台菜单表';
+
+-- ----------------------------
+-- Table structure for admin_resource
+-- ----------------------------
+CREATE TABLE `admin_resource` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL COMMENT '资源名称',
+  `url` varchar(200) DEFAULT NULL COMMENT '资源URL',
+  `description` varchar(500) DEFAULT NULL COMMENT '描述',
+  `category_id` bigint(20) DEFAULT NULL COMMENT '资源分类ID',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台资源表';
+
+-- ----------------------------
+-- Table structure for admin_resource_category
+-- ----------------------------
+CREATE TABLE `admin_resource_category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL COMMENT '分类名称',
+  `sort` int(4) DEFAULT 0 COMMENT '排序',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='资源分类表';
+
+-- ----------------------------
+-- Table structure for admin_role_menu_relation
+-- ----------------------------
+CREATE TABLE `admin_role_menu_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) DEFAULT NULL COMMENT '角色ID',
+  `menu_id` bigint(20) DEFAULT NULL COMMENT '菜单ID',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台角色菜单关系表';
+
+-- ----------------------------
+-- Table structure for admin_role_resource_relation
+-- ----------------------------
+CREATE TABLE `admin_role_resource_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `role_id` bigint(20) DEFAULT NULL COMMENT '角色ID',
+  `resource_id` bigint(20) DEFAULT NULL COMMENT '资源ID',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='后台角色资源关系表';
---
+++ sql/V0.0.3__Admin_product.sql
@@ -0,0 +1,151 @@
+-- ----------------------------
+-- Table structure for product
+-- ----------------------------
+CREATE TABLE `product` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `brand_id` bigint(20) DEFAULT NULL,
+  `product_category_id` bigint(20) DEFAULT NULL,
+  `feight_template_id` bigint(20) DEFAULT NULL,
+  `product_attribute_category_id` bigint(20) DEFAULT NULL,
+  `name` varchar(64) NOT NULL,
+  `pic` varchar(255) DEFAULT NULL,
+  `product_sn` varchar(64) NOT NULL COMMENT '货号',
+  `delete_status` int(1) DEFAULT NULL COMMENT '删除状态：0->未删除；1->已删除',
+  `publish_status` int(1) DEFAULT NULL COMMENT '上架状态：0->下架；1->上架',
+  `new_status` int(1) DEFAULT NULL COMMENT '新品状态:0->不是新品；1->新品',
+  `recommand_status` int(1) DEFAULT NULL COMMENT '推荐状态；0->不推荐；1->推荐',
+  `verify_status` int(1) DEFAULT NULL COMMENT '审核状态：0->未审核；1->审核通过',
+  `sort` int(11) DEFAULT NULL COMMENT '排序',
+  `sale` int(11) DEFAULT NULL COMMENT '销量',
+  `price` decimal(10,2) DEFAULT NULL,
+  `promotion_price` decimal(10,2) DEFAULT NULL COMMENT '促销价格',
+  `gift_growth` int(11) DEFAULT '0' COMMENT '赠送的成长值',
+  `gift_point` int(11) DEFAULT '0' COMMENT '赠送的积分',
+  `use_point_limit` int(11) DEFAULT NULL COMMENT '限制使用的积分数',
+  `sub_title` varchar(255) DEFAULT NULL COMMENT '副标题',
+  `description` text COMMENT '商品描述',
+  `original_price` decimal(10,2) DEFAULT NULL COMMENT '市场价',
+  `stock` int(11) DEFAULT NULL COMMENT '库存',
+  `low_stock` int(11) DEFAULT NULL COMMENT '库存预警值',
+  `unit` varchar(16) DEFAULT NULL COMMENT '单位',
+  `weight` decimal(10,2) DEFAULT NULL COMMENT '商品重量，默认为克',
+  `preview_status` int(1) DEFAULT NULL COMMENT '是否为预告商品：0->不是；1->是',
+  `service_ids` varchar(64) DEFAULT NULL COMMENT '以逗号分割的产品服务：1->无忧退货；2->快速退款；3->免费包邮',
+  `keywords` varchar(255) DEFAULT NULL,
+  `note` varchar(255) DEFAULT NULL,
+  `album_pics` varchar(255) DEFAULT NULL COMMENT '画册图片，连产品图片限制为5张，以逗号分割',
+  `detail_title` varchar(255) DEFAULT NULL,
+  `detail_desc` text,
+  `detail_html` text COMMENT '产品详情网页内容',
+  `detail_mobile_html` text COMMENT '移动端网页详情',
+  `promotion_start_time` datetime DEFAULT NULL COMMENT '促销开始时间',
+  `promotion_end_time` datetime DEFAULT NULL COMMENT '促销结束时间',
+  `promotion_per_limit` int(11) DEFAULT NULL COMMENT '活动限购数量',
+  `promotion_type` int(1) DEFAULT NULL COMMENT '促销类型：0->没有促销使用原价;1->使用促销价；2->使用会员价；3->使用阶梯价格；4->使用满减价格；5->限时购',
+  `brand_name` varchar(255) DEFAULT NULL COMMENT '品牌名称',
+  `product_category_name` varchar(255) DEFAULT NULL COMMENT '商品分类名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品信息';
+
+
+-- ----------------------------
+-- Table structure for product_attribute
+-- ----------------------------
+CREATE TABLE `product_attribute` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_attribute_category_id` bigint(20) DEFAULT NULL,
+  `name` varchar(64) DEFAULT NULL,
+  `select_type` int(1) DEFAULT NULL COMMENT '属性选择类型：0->唯一；1->单选；2->多选',
+  `input_type` int(1) DEFAULT NULL COMMENT '属性录入方式：0->手工录入；1->从列表中选取',
+  `input_list` varchar(255) DEFAULT NULL COMMENT '可选值列表，以逗号隔开',
+  `sort` int(11) DEFAULT NULL COMMENT '排序字段：最高的可以单独上传图片',
+  `filter_type` int(1) DEFAULT NULL COMMENT '分类筛选样式：1->普通；1->颜色',
+  `search_type` int(1) DEFAULT NULL COMMENT '检索类型；0->不需要进行检索；1->关键字检索；2->范围检索',
+  `related_status` int(1) DEFAULT NULL COMMENT '相同属性产品是否关联；0->不关联；1->关联',
+  `hand_add_status` int(1) DEFAULT NULL COMMENT '是否支持手动新增；0->不支持；1->支持',
+  `type` int(1) DEFAULT NULL COMMENT '属性的类型；0->规格；1->参数',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品属性参数表';
+
+-- ----------------------------
+-- Table structure for product_attribute_category
+-- ----------------------------
+CREATE TABLE `product_attribute_category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `attribute_count` int(11) DEFAULT '0' COMMENT '属性数量',
+  `param_count` int(11) DEFAULT '0' COMMENT '参数数量',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='产品属性分类表';
+
+-- ----------------------------
+-- Table structure for product_attribute_value
+-- ----------------------------
+CREATE TABLE `product_attribute_value` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) DEFAULT NULL,
+  `product_attribute_id` bigint(20) DEFAULT NULL,
+  `value` varchar(64) DEFAULT NULL COMMENT '手动添加规格或参数的值，参数单值，规格有多个时以逗号隔开',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='存储产品参数信息的表';
+
+-- ----------------------------
+-- Table structure for product_category
+-- ----------------------------
+CREATE TABLE `product_category` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '上机分类的编号：0表示一级分类',
+  `name` varchar(64) DEFAULT NULL,
+  `level` int(1) DEFAULT NULL COMMENT '分类级别：0->1级；1->2级',
+  `product_count` int(11) DEFAULT NULL,
+  `product_unit` varchar(64) DEFAULT NULL,
+  `nav_status` int(1) DEFAULT NULL COMMENT '是否显示在导航栏：0->不显示；1->显示',
+  `show_status` int(1) DEFAULT NULL COMMENT '显示状态：0->不显示；1->显示',
+  `sort` int(11) DEFAULT NULL,
+  `icon` varchar(255) DEFAULT NULL COMMENT '图标',
+  `keywords` varchar(255) DEFAULT NULL,
+  `description` text COMMENT '描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='产品分类';
+
+-- ----------------------------
+-- Table structure for product_category_attribute_relation
+-- ----------------------------
+CREATE TABLE `product_category_attribute_relation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_category_id` bigint(20) DEFAULT NULL,
+  `product_attribute_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='产品的分类和属性的关系表，用于设置分类筛选条件（只支持一级分类）';
+
+-- ----------------------------
+-- Table structure for product_operate_log
+-- ----------------------------
+CREATE TABLE `product_operate_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) DEFAULT NULL,
+  `price_old` decimal(10,2) DEFAULT NULL,
+  `price_new` decimal(10,2) DEFAULT NULL,
+  `sale_price_old` decimal(10,2) DEFAULT NULL,
+  `sale_price_new` decimal(10,2) DEFAULT NULL,
+  `gift_point_old` int(11) DEFAULT NULL COMMENT '赠送的积分',
+  `gift_point_new` int(11) DEFAULT NULL,
+  `use_point_limit_old` int(11) DEFAULT NULL,
+  `use_point_limit_new` int(11) DEFAULT NULL,
+  `operate_man` varchar(64) DEFAULT NULL COMMENT '操作人',
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='产品操作记录';
+
+-- ----------------------------
+-- Table structure for product_verify_record
+-- ----------------------------
+CREATE TABLE `product_verify_record` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `verify_man` varchar(64) DEFAULT NULL COMMENT '审核人',
+  `status` int(1) DEFAULT NULL,
+  `detail` varchar(255) DEFAULT NULL COMMENT '反馈详情',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品审核记录';
