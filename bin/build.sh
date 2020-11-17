fd2dcb8b86fae112ef3eed8ba0ab9379cd6638c7
senntyou <jiangjinbelief@163.com>
2020-11-17 12:28:18 +0800

    add bin/build.sh

---
+++ bin/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Usage: sh build.sh
+# 因为在开发服务器上经常更新代码，所以每次都用持续集成构建就不太方便
+# 一个解决方案是增量更新代码到服务器上，使用此脚本在服务器上构建
+
+set -e
+
+# 构建目录
+BUILD_DIR='build-project'
+# 项目名称
+PROJECT_NAME='pro-main'
+
+main(){
+  # change to build directory
+  echo "sh: cd ${BUILD_DIR}"
+  cd ${BUILD_DIR}
+
+  # execute script
+  echo "sh: ./gradlew ${PROJECT_NAME}:build"
+  ./gradlew ${PROJECT_NAME}:build
+
+  # move to ../libs
+  echo "sh: mv ${PROJECT_NAME}/build/libs/*.jar ../libs"
+  mv ${PROJECT_NAME}/build/libs/*.jar ../libs
+
+  # change to root directory
+  echo "sh: cd ../"
+  cd ../
+
+  echo 'sh: sh rerun.sh'
+  sh rerun.sh
+}
+
+main
