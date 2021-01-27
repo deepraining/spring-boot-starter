5ed174f62d524d68e5970e9dc5eec7935e677666
senntyou <jiangjinbelief@163.com>
2021-01-27 19:19:36 +0800

    pro-mbg add CommentGeneratorProperty.java

--- pro-mbg/src/main/java/senntyou/sbs/mbg/CommentGenerator.java
+++ pro-mbg/src/main/java/senntyou/sbs/mbg/CommentGenerator.java
@@ -1,25 +1,26 @@
 package senntyou.sbs.mbg;
 
-import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 import org.mybatis.generator.api.IntrospectedColumn;
 import org.mybatis.generator.api.IntrospectedTable;
 import org.mybatis.generator.api.dom.java.CompilationUnit;
 import org.mybatis.generator.api.dom.java.Field;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
 import org.mybatis.generator.internal.DefaultCommentGenerator;
 import org.mybatis.generator.internal.util.StringUtility;
 
 public class CommentGenerator extends DefaultCommentGenerator {
   // whether add column comment in database to java class fields by @ApiModelProperty
   private boolean addRemarkComments = false;
-  // fields to ignore when serialize object to json string
-  private String[] ignoreFields;
   private static final String EXAMPLE_SUFFIX = "Example";
   private static final String API_MODEL_PROPERTY_FULL_CLASS_NAME =
       "io.swagger.annotations.ApiModelProperty";
   private static final String JSON_IGNORE_FULL_CLASS_NAME =
       "com.fasterxml.jackson.annotation.JsonIgnore";
+  private static final String JSON_FILTER_FULL_CLASS_NAME =
+      "com.fasterxml.jackson.annotation.JsonFilter";
 
   // to generate position of @ApiModelProperty
   private String currentTable;
@@ -29,10 +30,6 @@ public class CommentGenerator extends DefaultCommentGenerator {
   public void addConfigurationProperties(Properties properties) {
     super.addConfigurationProperties(properties);
     this.addRemarkComments = StringUtility.isTrue(properties.getProperty("addRemarkComments"));
-    String ignoreFieldsString = properties.getProperty("ignoreFields");
-    if (ignoreFieldsString != null && ignoreFieldsString.length() > 0) {
-      ignoreFields = ignoreFieldsString.split(",");
-    }
   }
 
   @Override
@@ -59,8 +56,17 @@ public class CommentGenerator extends DefaultCommentGenerator {
           "@ApiModelProperty(value = \"" + remarks + "\", position = " + fieldIndex + ")");
     }
 
-    if (ignoreFields != null && ignoreFields.length > 0) {
-      if (Arrays.binarySearch(ignoreFields, field.getName()) > -1) {
+    List<String> commonIgnoreFields = CommentGeneratorProperty.commonIgnoreFields;
+    if (commonIgnoreFields != null && commonIgnoreFields.size() > 0) {
+      if (commonIgnoreFields.contains(field.getName())) {
+        field.addJavaDocLine("@JsonIgnore");
+        return;
+      }
+    }
+
+    List<String> specIgnoreFields = CommentGeneratorProperty.specIgnoreFields.get(table);
+    if (specIgnoreFields != null && specIgnoreFields.size() > 0) {
+      if (specIgnoreFields.contains(field.getName())) {
         field.addJavaDocLine("@JsonIgnore");
       }
     }
@@ -89,6 +95,27 @@ public class CommentGenerator extends DefaultCommentGenerator {
 
       // JsonIgnore
       compilationUnit.addImportedType(new FullyQualifiedJavaType(JSON_IGNORE_FULL_CLASS_NAME));
+      // JsonFilter
+      compilationUnit.addImportedType(new FullyQualifiedJavaType(JSON_FILTER_FULL_CLASS_NAME));
+    }
+  }
+
+  @Override
+  public void addModelClassComment(
+      TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
+    super.addClassComment(topLevelClass, introspectedTable);
+
+    String table = introspectedTable.getFullyQualifiedTable().toString();
+
+    String commonFilterName = CommentGeneratorProperty.commonFilterName;
+    if (commonFilterName != null && commonFilterName.length() > 0) {
+      topLevelClass.addJavaDocLine("@JsonFilter(\"" + commonFilterName + "\")");
+      return;
+    }
+
+    String specFilterName = CommentGeneratorProperty.specFilterName.get(table);
+    if (specFilterName != null && specFilterName.length() > 0) {
+      topLevelClass.addJavaDocLine("@JsonFilter(\"" + specFilterName + "\")");
     }
   }
 }
