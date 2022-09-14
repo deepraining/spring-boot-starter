92bb133ca872240747a6fed2284d15f018e2379f
senntyou <jiangjinbelief@163.com>
2022-09-14 12:05:53 +0800

    update

--- pro-admin/src/main/java/dr/sbs/admin/component/GlobalExceptionHandler.java
+++ pro-admin/src/main/java/dr/sbs/admin/component/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 // 全局异常处理
 // 注：把这个文件复制到每项目的主包名下，才能生效
+// 或者启动时设置 `@SpringBootApplication(scanBasePackages = {"dr.sbs"})`
 @ControllerAdvice
 public class GlobalExceptionHandler {
   @ResponseBody
